28c1c3de564039dcf6131f02e69fb3e04ad26252
MVDDMN <delacruzkp@students.national-u.edu.ph>
2025-10-18 15:40:20 +0800

    Design: Overhauled the design of the portfolio.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,12 @@ import './Hero.css';
 
 const Hero: React.FC = () => (
     <section id="hero" className="portfolio-hero">
-        <img src={profileImg} alt="Profile" className="profile-icon" />
+        <img
+            src={profileImg}
+            alt="Profile"
+            className="profile-icon"
+            onContextMenu={(e) => e.preventDefault()}
+        />
         <h1>Karl Angelo P. Dela Cruz</h1>
         <p>Fullstack Developer, Specializing in Mobile and Web Development</p>
     </section>
